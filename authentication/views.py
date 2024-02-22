574dec2cc21d6a2b1c10958c0880351abe2adfca
Pankaj Padekar <padekarpankaj@gmail.com>
2024-02-22 18:06:24 +0530

    added Sign in page

--- authentication/views.py
+++ authentication/views.py
@@ -1,18 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
+
 
 def home(request):
     template = loader.get_template("index.html")
     return render(request, "index.html")
 
+
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        myuser = User.objects.create_user(
+            username, email, password)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(
+            request, "Your account has been successfully created.")
+
+        return redirect('signin')
+
     return render(request, "signup.html")
 
+
 def signin(request):
-    return render(request, "signuin.html")
+    return render(request, "signin.html")
+
 
 def signout(request):
     pass
